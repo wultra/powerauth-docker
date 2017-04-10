e006c1c7255f00fe79a7b2f9bc62c9d29f6f3f50
Petr Dvorak <petr@inmite.eu>
2017-04-10 21:54:26 +0200

    Add extra pull command to allow repeated script launch in the same folder

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/lime-company/lime-security-powerauth-docker.git ; \
 cd lime-security-powerauth-docker ; \
+git pull ; \
 sh build.sh ; \
 docker-compose up -d ; \
 cd .. ;
