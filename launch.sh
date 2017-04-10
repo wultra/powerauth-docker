2ed9353c303035cb73e2b051f8cbaf84d8b55ff8
Petr Dvorak <petr@inmite.eu>
2017-04-10 21:52:40 +0200

    Remove the redundant 'open' commands

--- launch.sh
+++ launch.sh
@@ -2,7 +2,4 @@ git clone https://github.com/lime-company/lime-security-powerauth-docker.git ; \
 cd lime-security-powerauth-docker ; \
 sh build.sh ; \
 docker-compose up -d ; \
-cd ..; \
-sleep 10 ; \
-open http://localhost:18080/powerauth-admin ; \
-open http://localhost:28080/powerauth-push-server
+cd .. ;
