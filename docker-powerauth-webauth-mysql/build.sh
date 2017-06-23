651a034e8288de0928902a3a7ba3b494661d975d
Petr Dvorak <petr@inmite.eu>
2017-06-23 15:21:17 +0200

    Activate all methods for user 12345678

--- docker-powerauth-webauth-mysql/build.sh
+++ docker-powerauth-webauth-mysql/build.sh
@@ -4,6 +4,7 @@ mkdir ./build
 cp ./conf/schema.sql ./build/schema.sql
 curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth-webauth/master/powerauth-webauth-docs/sql/mysql/create_schema.sql >> ./build/schema.sql
 curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth-webauth/master/powerauth-webauth-docs/sql/mysql/initial_data.sql >> ./build/schema.sql
+cat ./conf/schema.sql >> ./build/schema.sql
 
 docker build -t powerauth-webauth-mysql ./
 
---
+++ docker-powerauth-webauth-mysql/conf/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO ns_user_prefs (user_id, auth_method_1, auth_method_2, auth_method_3, auth_method_4, auth_method_5) VALUES ('12345678', 1, 1, 1, 1, 1);
