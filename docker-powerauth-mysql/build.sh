dd46f6704cc7fd27a64a3f2881801e7333e75da6
Petr Dvorak <petr@inmite.eu>
2017-08-20 13:35:33 +0200

    Fixed paths

--- docker-powerauth-mysql/build.sh
+++ docker-powerauth-mysql/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir ./build
-curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth/master/powerauth-docs/sql/mysql/schema_boot.sql > ./build/schema.sql
-curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth/master/powerauth-docs/sql/mysql/create_schema.sql >> ./build/schema.sql
+curl -L https://raw.githubusercontent.com/lime-company/powerauth-server/master/docs/sql/mysql/schema_boot.sql > ./build/schema.sql
+curl -L https://raw.githubusercontent.com/lime-company/powerauth-server/master/docs/sql/mysql/create_schema.sql >> ./build/schema.sql
 
 docker build -t powerauth-mysql ./
 
--- docker-powerauth-push-mysql/build.sh
+++ docker-powerauth-push-mysql/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir ./build
-curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth/master/powerauth-docs/sql/mysql/schema_boot.sql > ./build/schema.sql
-curl -L https://raw.githubusercontent.com/lime-company/lime-security-powerauth-push/master/powerauth-push-docs/sql/mysql/create_push_server_schema.sql >> ./build/schema.sql
+curl -L https://raw.githubusercontent.com/lime-company/powerauth-server/master/docs/sql/mysql/schema_boot.sql > ./build/schema.sql
+curl -L https://raw.githubusercontent.com/lime-company/powerauth-push-server/master/powerauth-push-docs/sql/mysql/create_push_server_schema.sql >> ./build/schema.sql
 
 docker build -t powerauth-push-mysql ./
 
