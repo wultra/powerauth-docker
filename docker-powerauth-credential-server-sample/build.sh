1cf1e83aaf19ba1ee08440547d6b1da003b899dd
Petr Dvorak <petr@inmite.eu>
2017-08-20 14:06:54 +0200

    Add MySQL lib to classpath

--- docker-powerauth-credential-server-sample/build.sh
+++ docker-powerauth-credential-server-sample/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir ./build
+curl -L http://central.maven.org/maven2/mysql/mysql-connector-java/$MYSQL_VERSION/mysql-connector-java-$MYSQL_VERSION.jar > ./build/mysql-connector-java.jar
 curl -L https://dl.dropboxusercontent.com/u/6405782/tmp_pa_development/powerauth-credential-server-sample.war > ./build/powerauth-credential-server-sample.war
 
 docker build -t powerauth-credential-server-sample ./
