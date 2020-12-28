d95ca23f0e18329b2358a45a981e3e8ed4e00dff
Lukáš Lukovský <lukas.lukovsky@wultra.com>
2020-12-28 14:15:16 +0100

    Fix nginx run

--- deploy/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-liquibase --changeLogFile=$LIQUIBASE_HOME/data/changelog.xml --username=$POWERAUTH_SERVER_DATASOURCE_USERNAME --password=$POWERAUTH_SERVER_DATASOURCE_PASSWORD --url=$POWERAUTH_SERVER_DATASOURCE_URL update
-
-exec "$@"
